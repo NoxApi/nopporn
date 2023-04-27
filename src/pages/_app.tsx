9b3ea35cce70c70e05b40cb43206468d9f22ec3f
NoxApi <pcsnon@gmail.com>
2023-04-27 18:07:25 +0700

    add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Footer } from '@/components/general/footer'
 import { Navbar } from '@/components/general/navbar'
+import Script from 'next/script'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className='w-full flex justify-center relative'>
@@ -10,6 +11,19 @@ export default function App({ Component, pageProps }: AppProps) {
       rel="stylesheet"
       />
       <link href='https://fonts.googleapis.com/css?family=Noto Sans Thai' rel='stylesheet'/>
+      <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-T59LHBSCPN"
+                strategy="afterInteractive"
+              />
+              <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', 'G-HEVZ5S15LM');
+                `}
+      </Script>
       <div className=" flex flex-col items-center opensans h-auto w-[1920px] justify-center overflow-hidden">
         <Navbar/>
         <Component {...pageProps} />
